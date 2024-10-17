f501d6caf37c9dfcd3627bbe44c6f73116bd9e75
Alex Ahumada <projectsperminute@gmail.com>
2024-10-17 08:17:41 +0200

    Add button and fix transition for Orgs component

--- components/landing/Orgs.vue
+++ components/landing/Orgs.vue
@@ -17,14 +17,17 @@ const { orgs = [] } = defineProps<{
 </script>
 
 <template>
-  <div class="grid md:grid-cols-3 gap-20 mx-auto my-20">
-    <div v-for="item of orgs" class="hover:-translate-y-1">
+  <div class="grid md:grid-cols-3 gap-10 mx-auto my-20">
+    <div
+      v-for="item of orgs"
+      class="p-10 rounded-lg bg-none hover:bg-white transition-all"
+    >
       <a :href="item.url" target="_blank">
         <div class="w-full mb-8">
           <img
             :src="item.avatar.src"
             :alt="item.alt"
-            class="w-full h-full object-contain rounded transition"
+            class="w-full h-full object-contain rounded"
           />
         </div>
 
@@ -33,6 +36,11 @@ const { orgs = [] } = defineProps<{
             {{ item.name }}
           </h2>
           <p class="text-lg text-slate-700">{{ item.description }}</p>
+        </div>
+        <div class="mt-6 flex flex-col sm:flex-row gap-3">
+          <LandingLink :href="item.url" target="_blank" rel="noopener"
+            >Ver más</LandingLink
+          >
         </div>
       </a>
     </div>
