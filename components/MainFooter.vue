6dc86d3df153ca421b46ea5f617d8b19882567ca
Alex Ahumada <projectsperminute@gmail.com>
2024-10-18 10:02:57 +0200

    Adds footer contact details

--- components/MainFooter.vue
+++ components/MainFooter.vue
@@ -10,5 +10,16 @@
         Aviso legal.
       </NuxtLinkLocale>
     </p>
+    <p
+      class="flex justify-center items-center text-center text-sm text-slate-200"
+    >
+      <Icon name="bx:pin" class="mx-1" />
+      Cardenal Herrera Oria 63, 3 IZDA, 28047, Madrid |
+      <Icon name="bx:phone" class="mx-1" /> 917 39 52 57 |
+      <Icon name="bx:mail-send" class="mx-1" /><a
+        href="mailto:coordinacion@unmundosalvadorsoler.org"
+        >coordinacion@unmundosalvadorsoler.org</a
+      >
+    </p>
   </footer>
 </template>
